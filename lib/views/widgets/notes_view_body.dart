b3a02f0a37110abfd480740502aba760c25914a2
ragehh <mrageh68@gmail.com>
2025-08-15 16:16:00 +0300

    create custom app bar

---
+++ lib/views/widgets/notes_view_body.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+import 'custom_app_bar.dart';
+
+class NotesViewBody extends StatelessWidget {
+  const NotesViewBody({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 24),
+      child: const Column(children: [CustomAppBar()]),
+    );
+  }
+}
