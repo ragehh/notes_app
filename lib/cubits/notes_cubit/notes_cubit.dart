07d052935a12f9168df048ea75a9e606ce2e417f
ragehh <mrageh68@gmail.com>
2025-08-20 16:10:42 +0300

    create notes state

---
+++ lib/cubits/notes_cubit/notes_cubit.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:notes_app/models/note_model.dart';
+
+part 'notes_state.dart';
+
+class NotesCubit extends Cubit<NotesState> {
+  NotesCubit() : super(NotesInitial());
+}
