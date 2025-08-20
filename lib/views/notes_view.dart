cb0ae6149ac8a151e68bc90cd38b79aa3390b808
ragehh <mrageh68@gmail.com>
2025-08-20 15:10:14 +0300

    fix issue: show keyboard in addNote bottom sheet

--- lib/views/notes_view.dart
+++ lib/views/notes_view.dart
@@ -13,6 +13,7 @@ class NotesView extends StatelessWidget {
         backgroundColor: kPrimaryColor,
         onPressed: () {
           showModalBottomSheet(
+            isScrollControlled: true,
             shape: RoundedRectangleBorder(
               borderRadius: BorderRadius.circular(16),
             ),
